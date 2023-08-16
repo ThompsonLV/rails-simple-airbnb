2bcb71059a8215aec804633eb60e6dd0bdb0a8d5
Thomas Le Veo <thomasleveo@gmail.com>
2023-08-17 00:04:37 +0200

    Flats : index, show, new, create, delete, css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+puts '-------------------'
+puts 'Destroying all data'
+puts '-------------------'
+Flat.destroy_all
+
+puts 'Creating data'
+puts '-------------------'
+15.times do
+  Flat.create!(
+    name: Faker::Markdown.emphasis,
+    address: Faker::Address.full_address,
+    description: Faker::Address.country,
+    price_per_night: (50..100).to_a.sample,
+    number_of_guests: (1..10).to_a.sample
+  )
+end
+
+puts '---- Finished -----'
+puts '-------------------'
